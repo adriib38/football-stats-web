5209af8d01a8f5c0627f1be239bf286a2ffcdf91
adriib38 <adrianbenitezb7@gmail.com>
2025-01-10 20:06:46 +0100

    Add credentials include

--- src/services/StatsApi.jsx
+++ src/services/StatsApi.jsx
@@ -1,9 +1,12 @@
 const API_BASE_URL = "https://urchin-app-jtmde.ondigitalocean.app";
+//const API_BASE_URL = "http://localhost:3005";
+
 console.log("API_BASE_URL: " +API_BASE_URL)
 export const getClassification = async(competition) => {
     const url = `${API_BASE_URL}/${competition}/classification`;
     const resp = await fetch(url, {
         method: "GET",
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
         },
@@ -18,6 +21,7 @@ export const getStats = async(competition) => {
     const url = `${API_BASE_URL}/${competition}/stats`;
     const resp = await fetch(url, {
         method: "GET",
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
         },
@@ -32,6 +36,7 @@ export const getGames = async(competition) => {
     const url = `${API_BASE_URL}/${competition}/games`;
     const resp = await fetch(url, {
         method: "GET",
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
         },
