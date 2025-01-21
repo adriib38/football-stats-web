5b73eb627018a92b74e72b8fcafc7c75180a2da0
adriib38 <adrianbenitezb7@gmail.com>
2025-01-21 22:35:03 +0100

    Add build

--- src/services/TeamApi.jsx
+++ src/services/TeamApi.jsx
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:3005/t";
+// const API_BASE_URL = "http://localhost:3005/t";
+const API_BASE_URL = "https://seal-app-myhre.ondigitalocean.app/t";
 
 export const getTeam = async(team) => {
     const url = `${API_BASE_URL}/${team}`;
